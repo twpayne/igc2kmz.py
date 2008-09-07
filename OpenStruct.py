b6e40cf78c3730b64fd6c512e10dba49a2b6c9d3
Tom Payne <twp@sol.(none)>
2008-09-07 14:52:49 +0200

    Remove unnecessary functions.

--- OpenStruct.py
+++ OpenStruct.py
@@ -1,17 +1,7 @@
-class OpenStruct:
+class OpenStruct(object):
 
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs)
-
-  def __getattr__(self, key):
-    if self.__dict__.has_key(key):
-      return self.__dict__[key]
-    else:
-      raise AttributeError, key
-
-  def __setattr__(self, key, value):
-    self.__dict__[key] = value
-    return value
 
   def __repr__(self):
     return 'OpenStruct(%s)' % ', '.join(['%s=%s' % (key, repr(value)) for key, value in self.__dict__.items()])
