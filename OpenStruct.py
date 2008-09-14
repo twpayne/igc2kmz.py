8b772e518b5de5246c33d44464cdb53fa5cdbb5f
Tom Payne <twpayne@gmail.com>
2008-09-14 11:43:28 +0200

    Clean-up.

--- OpenStruct.py
+++ OpenStruct.py
@@ -3,5 +3,8 @@ class OpenStruct(object):
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs)
 
+  def __iter__(self):
+    return self.__dict__.iteritems()
+
   def __repr__(self):
-    return 'OpenStruct(%s)' % ', '.join(['%s=%s' % (key, repr(value)) for key, value in self.__dict__.items()])
+    return 'OpenStruct(%s)' % ', '.join('%s=%s' % (key, repr(value)) for key, value in self)
