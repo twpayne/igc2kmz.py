9f420c321735b4680a6e1dda77d395994b11b19e
Tom Payne <twpayne@gmail.com>
2008-10-12 21:56:03 +0200

    Reformat.

--- igc2kmz/coord.py
+++ igc2kmz/coord.py
@@ -23,57 +23,69 @@ compass = 'N NNE NE ENE E ESE SE SSE S SSW SW WSW W WNW NW NNW'.split()
 
 
 def rad_to_compass(rad):
-  while rad < 0.0:
-    rad += 2 * pi
-  return compass[int(8 * rad / pi + 0.5) % 16]
+    while rad < 0.0:
+        rad += 2 * pi
+    return compass[int(8 * rad / pi + 0.5) % 16]
 
 
 class Coord(object):
 
-  __slots__ = ('lat', 'lon', 'ele', 'dt')
+    __slots__ = ('lat', 'lon', 'ele', 'dt')
 
-  def __init__(self, lat, lon, ele, dt=None):
-    self.lat = lat
-    self.lon = lon
-    self.ele = ele
-    self.dt = dt
+    def __init__(self, lat, lon, ele, dt=None):
+        self.lat = lat
+        self.lon = lon
+        self.ele = ele
+        self.dt = dt
 
-  @classmethod
-  def deg(cls, lat, lon, ele, dt=None):
-    return cls(pi * lat / 180.0, pi * lon / 180.0, ele, dt)
+    @classmethod
+    def deg(cls, lat, lon, ele, dt=None):
+        return cls(pi * lat / 180.0, pi * lon / 180.0, ele, dt)
 
-  def initial_bearing_to(self, other):
-    """Return the initial bearing from self to other."""
-    return atan2(sin(other.lon - self.lon) * cos(other.lat), cos(self.lat) * sin(other.lat) - sin(self.lat) * cos(other.lat) * cos(other.lon - self.lon))
+    def initial_bearing_to(self, other):
+        """Return the initial bearing from self to other."""
+        y = sin(other.lon - self.lon) * cos(other.lat)
+        x = cos(self.lat) * sin(other.lat) \
+            - sin(self.lat) * cos(other.lat) * cos(other.lon - self.lon)
+        return atan2(y, x)
 
-  def distance_to(self, other):
-    """Return the distance from self to other."""
-    d = sin(self.lat) * sin(other.lat) + cos(self.lat) * cos(other.lat) * cos(self.lon - other.lon)
-    return R * acos(d) if d < 1.0 else 0.0
+    def distance_to(self, other):
+        """Return the distance from self to other."""
+        d = sin(self.lat) * sin(other.lat) \
+            + cos(self.lat) * cos(other.lat) * cos(self.lon - other.lon)
+        return R * acos(d) if d < 1.0 else 0.0
 
-  def halfway_to(self, other):
-    """Return the point halfway between self and other."""
-    bx = cos(other.lat) * cos(other.lon - self.lon)
-    by = cos(other.lat) * sin(other.lon - self.lon)
-    cos_lat_plus_bx = cos(self.lat) + bx
-    lat = atan2(sin(self.lat) + sin(other.lat), sqrt(cos_lat_plus_bx * cos_lat_plus_bx + by * by))
-    lon = self.lon + atan2(by, cos_lat_plus_bx)
-    ele = (self.ele + other.ele) / 2.0
-    return Coord(lat, lon, ele)
+    def halfway_to(self, other):
+        """Return the point halfway between self and other."""
+        bx = cos(other.lat) * cos(other.lon - self.lon)
+        by = cos(other.lat) * sin(other.lon - self.lon)
+        cos_lat_plus_bx = cos(self.lat) + bx
+        lat = atan2(sin(self.lat) + sin(other.lat),
+                    sqrt(cos_lat_plus_bx * cos_lat_plus_bx + by * by))
+        lon = self.lon + atan2(by, cos_lat_plus_bx)
+        ele = (self.ele + other.ele) / 2.0
+        return Coord(lat, lon, ele)
 
-  def interpolate(self, other, delta):
-    """Return the point delta between self and other."""
-    d = sin(self.lat) * sin(other.lat) + cos(self.lat) * cos(other.lat) * cos(other.lon - self.lon)
-    d = delta * acos(d) if d < 1.0 else 0.0
-    theta = atan2(sin(other.lon - self.lon) * cos(other.lat), cos(self.lat) * sin(other.lat) - sin(self.lat) * cos(other.lat) * cos(other.lon - self.lon))
-    lat = asin(sin(self.lat) * cos(d) + cos(self.lat) * sin(d) * cos(theta))
-    lon = self.lon + atan2(sin(theta) * sin(d) * cos(self.lat), cos(d) - sin(self.lat) * sin(lat))
-    ele = (1.0 - delta) * self.ele + delta * other.ele
-    return Coord(lat, lon, ele)
+    def interpolate(self, other, delta):
+        """Return the point delta between self and other."""
+        d = sin(self.lat) * sin(other.lat) \
+            + cos(self.lat) * cos(other.lat) * cos(other.lon - self.lon)
+        d = delta * acos(d) if d < 1.0 else 0.0
+        y = sin(other.lon - self.lon) * cos(other.lat)
+        x = cos(self.lat) * sin(other.lat) \
+            - sin(self.lat) * cos(other.lat) * cos(other.lon - self.lon)
+        theta = atan2(y, x)
+        lat = asin(sin(self.lat) * cos(d) + cos(self.lat) * sin(d) * cos(theta))
+        lon = self.lon + atan2(sin(theta) * sin(d) * cos(self.lat),
+                               cos(d) - sin(self.lat) * sin(lat))
+        ele = (1.0 - delta) * self.ele + delta * other.ele
+        return Coord(lat, lon, ele)
 
-  def coord_at(self, theta, d):
-    """Return the point d from self in direction theta."""
-    lat = asin(sin(self.lat) * cos(d / R) + cos(self.lat) * sin(d / R) * cos(theta))
-    lon = self.lon + atan2(sin(theta) * sin(d / R) * cos(self.lat), cos(d / R) - sin(self.lat) * sin(lat))
-    ele = self.ele
-    return Coord(lat, lon, ele)
+    def coord_at(self, theta, d):
+        """Return the point d from self in direction theta."""
+        lat = asin(sin(self.lat) * cos(d / R) \
+                   + cos(self.lat) * sin(d / R) * cos(theta))
+        lon = self.lon + atan2(sin(theta) * sin(d / R) * cos(self.lat),
+                               cos(d / R) - sin(self.lat) * sin(lat))
+        ele = self.ele
+        return Coord(lat, lon, ele)
